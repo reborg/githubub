6a87c049bf401fb8e8cfbdb00bb7abe082dbc3d8
reborg <reborg@reborg.net>
2010-11-03 22:01:15 +0000

    removed signup with email and password and fixed github oauth workflow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,25 +1,17 @@
 class HomeController < ApplicationController
   before_filter :authenticate_user!, :except => [:authenticate]
 
-  def index
-  end
-
   def authenticate
     #render :text => request.env["omniauth.auth"].to_yaml
     omniauth = request.env["omniauth.auth"]
     if current_user
-      flash[:notice] = "Authentication successful."
-      redirect_to dashboard_url
+      flash[:notice] = "User already authenticated succesfully."
+      redirect_to root_url
     else
-      user = User.new
-      user.apply_omniauth(omniauth)
-      if user.save
-        flash[:notice] = "Signed in successfully."
-        sign_in_and_redirect(:user, user)
-      else
-        session[:omniauth] = omniauth.except('extra')
-        redirect_to new_user_registration_url
-      end
+      email = omniauth['user_info']['email']
+      user = User.find_by_email(email) || User.create(:email => email)
+      flash[:notice] = "Signed in successfully."
+      sign_in_and_redirect(:user, user)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Bgd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "dashboard#show"
 
   # See how all your routes lay out with "rake routes"
 
